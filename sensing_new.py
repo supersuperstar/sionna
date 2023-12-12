e4dae0be8e681296f46c1d4f4061270c01f0ddf5
supersuperstar <1652112417@qq.com>
2023-12-12 18:50:01 +0800

    同步sionna库

--- sensing_new.py
+++ sensing_new.py
@@ -20,13 +20,10 @@ from sionna.mimo import StreamManagement
 
 from sionna.ofdm import ResourceGrid, ResourceGridMapper, LSChannelEstimator, LMMSEEqualizer
 from sionna.ofdm import OFDMModulator, OFDMDemodulator, ZFPrecoder, RemoveNulledSubcarriers
-
-from sionna.channel.tr38901 import AntennaArray, CDL, Antenna
 from sionna.channel import subcarrier_frequencies, cir_to_ofdm_channel, cir_to_time_channel, time_lag_discrete_time_channel
 from sionna.channel import ApplyOFDMChannel, ApplyTimeChannel, OFDMChannel, TimeChannel
 
 from sionna.fec.ldpc.encoding import LDPC5GEncoder
-from sionna.fec.ldpc.decoding import LDPC5GDecoder
 
 from sionna.mapping import Mapper, Demapper
 
@@ -35,20 +32,37 @@ from sionna.utils.metrics import compute_ber
 # Import Sionna RT components
 from sionna.rt import load_scene, Transmitter, Receiver, PlanarArray, Camera
 
-
-class Scene():
+class SensingScene():
     def __init__(self) -> None:
         self._scene = load_scene(sionna.rt.scene.munich) # Try also sionna.rt.scene.etoile
         self._scene.frequency = 2.14e9 # in Hz; implicitly updates RadioMaterials
         self._scene.synthetic_array = True # If set to False, ray tracing will be done per antenna element (slower for large arrays)
+        self._scene.tx_array = PlanarArray(num_rows=1,
+                             num_cols=1,
+                             vertical_spacing=0.5,
+                             horizontal_spacing=0.5,
+                             pattern="dipole",
+                             polarization="V")
+    # Configure antenna array for all receivers
+        self._scene.rx_array = PlanarArray(num_rows=1,
+                                num_cols=1,
+                                vertical_spacing=0.5,
+                                horizontal_spacing=0.5,
+                                pattern="dipole",
+                                polarization="V")
+        tx = Transmitter(name="tx",
+                 position=[8.5,21,27])
+        self._scene.add(tx)
+        self._cm = self._scene.coverage_map()
 
 def main(**kwargs):
     # Set up the simulation parameters
     # ------------
-    pass
+    scene = SensingScene()
     
     
 
-if __name__ == "main":
-    
+if __name__ == "__main__":
+    print("Starting simulation...")
     main()
+    print("Simulation finished.")
