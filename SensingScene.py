86bad98b26fd1507c74f1e975a640307e955b228
supersuperstar <1652112417@qq.com>
2023-12-18 15:27:09 +0800

    更新了SensingScene用法

--- SensingScene.py
+++ SensingScene.py
@@ -12,7 +12,8 @@ class SensingScene():
         self.crb_speed = None
         self._cell_positions = None # 3D array of cell positions
         self._cell_map = None # 2D array of cell map
-        self._cell_num = 1
+        self._cell_num_x = 1
+        self._cell_num_y = 1
         self._paths = None
         self._tx_positions = tx_positions
         self._tx_look_dir = tx_look_dir
@@ -41,30 +42,24 @@ class SensingScene():
                                     polarization="V")
         print("Scene initialized.")
     
-    def get_cell_positions(self,map_position,map_size, cell_size):
-        cell_num = int(map_size/cell_size) # Number of cells in the map
-        self.cell_num = cell_num
-        self._cell_positions = np.zeros((cell_num, cell_num, 3))
-        self._cell_map = np.zeros((cell_num, cell_num))
-        self._cell_positions[0,0,:] = map_position # Set the first cell position
-        self.map_position = map_position
-        self.map_size = map_size
-        self.cell_size = cell_size
-        for i in range(0, cell_num):
-            # Set the first cell position of each row
-            if i > 0:
-                self._cell_positions[i,0,:] = self._cell_positions[i-1,0,:] + [cell_size,0,0]
-            for j in range(1, cell_num):
-                self._cell_positions[i,j,:] = self._cell_positions[i,j-1,:] + [0,cell_size,0]
+    def get_cell_positions(self,map_center,map_size_x,map_size_y, cell_size):
+        cell_num_x = int(map_size_x/cell_size) + 1 # Number of cells in the map
+        cell_num_y = int(map_size_y/cell_size) + 1 # Number of cells in the map
+        self._cell_num_x = cell_num_x
+        self._cell_num_y = cell_num_y
+        self._map_center = map_center
+        self._map_size_x = map_size_x
+        self._map_size_y = map_size_y
+        self._cell_size = cell_size
+        self._cell_positions = np.zeros((cell_num_x, cell_num_y, 3))
+        x_fill = np.arange(0,cell_num_x) * cell_size + map_center[0] - map_size_x/2
+        x_fill = np.tile(x_fill,cell_num_y)
+        self._cell_positions[:,:,0] = x_fill.reshape([cell_num_y,cell_num_x]).transpose()
+        y_fill = np.arange(0,cell_num_y) * cell_size + map_center[1] - map_size_y/2
+        y_fill = np.tile(y_fill,cell_num_x)
+        self._cell_positions[:,:,1] = y_fill.reshape([cell_num_x,cell_num_y])
+        self._cell_positions[:,:,2] = np.tile(map_center[2],(cell_num_x,cell_num_y))
         return self._cell_positions
-    
-    @property
-    def cell_num(self):
-        return self._cell_num
-    
-    @cell_num.setter
-    def cell_num(self,cell_num):
-        self._cell_num = cell_num
 
     def set_tx(self,tx_positions,tx_look_dir):
         """_summary_
@@ -81,11 +76,12 @@ class SensingScene():
         self._tx_look_dir = np.array(tx_look_dir)
     
     def del_receiver(self):
-        cell_num = self.cell_num
-        if cell_num is None:
+        cell_num_x = self._cell_num_x
+        cell_num_y = self._cell_num_y
+        if cell_num_x is None or cell_num_y is None:
             return
-        for i in range(0, cell_num):
-            for j in range(0, cell_num):
+        for i in range(0, cell_num_x):
+            for j in range(0, cell_num_y):
                 rx_name = f"rx_{i}_{j}"
                 if self._scene.get(rx_name) is not None:
                     self._scene.remove(rx_name)
@@ -93,67 +89,11 @@ class SensingScene():
     def compute_paths(self,max_depth=3,num_samples=1000000,max_rx_each_turn = 100,los=True,reflection=True,diffraction=False,scattering=False):
         self._paths = None
         scene = self._scene
-        cell_num = self.cell_num # Number of cells in the map
+        cell_num_x = self._cell_num_x # Number of cells in the map
+        cell_num_y = self._cell_num_y # Number of cells in the map
         cell_positions = self._cell_positions # 3D array of cell positions
         num_tx = self._tx_positions.shape[0] # Number of transmitters
-        for tx_idx in range(0,num_tx):
-            # remove previous transmitter
-            if scene.get(f"tx_{tx_idx-1}") is not None:
-                scene.remove(f"tx_{tx_idx-1}")
-            # Add new transmitter
-            tx = Transmitter(name=f"tx_{tx_idx}",position=self._tx_positions[tx_idx,:])
-            if scene.get(f"tx_{tx_idx}") is not None:
-                scene.remove(f"tx_{tx_idx}")
-            scene.add(tx)
-            num = 0
-            i = 0
-            rx_name_list = []
-            # Compute paths for receivers
-            while i < cell_num:
-                j = 0
-                while j < cell_num:
-                    # Add receiver
-                    rx_name = f"rx_{i}_{j}"
-                    rx_name_list.append(rx_name)
-                    rx = Receiver(name=rx_name,position=cell_positions[i,j,:])
-                    if scene.get(rx_name) is not None:
-                        scene.remove(rx_name)
-                    scene.add(rx)
-                    num += 1
-                    if num == max_rx_each_turn: # Compute paths
-                        num = 0
-                        paths:mysionna.rt.Paths = scene.compute_paths(max_depth=max_depth,num_samples=num_samples,los=los,reflection=reflection,diffraction=diffraction,scattering=scattering)
-                        if self._paths is None:
-                            self._paths = paths
-                        else:
-                            self._paths = self._paths.merge_different_rx(paths)
-                        for rx_name in rx_name_list:
-                            scene.remove(rx_name)
-                        rx_name_list = []
-                    j += 1
-                i += 1
-            # Compute paths for the rest receivers
-            if num > 0:
-                paths:mysionna.rt.Paths = scene.compute_paths(max_depth=max_depth,num_samples=num_samples,los=los,reflection=reflection,diffraction=diffraction,scattering=scattering)
-                if self._paths is None:
-                    self._paths = paths
-                else:
-                    self._paths = self._paths.merge_different_rx(paths)
-                for rx_name in rx_name_list:
-                    scene.remove(rx_name)
-                rx_name_list = []
-        
-        self._paths.finalize_different_rx()
-        
-        return self._paths
-    
-    def compute_paths_crb(self,max_depth=3,num_samples=1000000,max_rx_each_turn = 100,los=True,reflection=True,diffraction=False,scattering=False):
-        self._paths = None
-        scene = self._scene
-        cell_num = self.cell_num # Number of cells in the map
-        cell_positions = self._cell_positions # 3D array of cell positions
-        num_tx = self._tx_positions.shape[0] # Number of transmitters
-        self.crb_delay = np.zeros((num_tx,cell_num,cell_num))
+        self.crb_delay = np.zeros((num_tx,cell_num_x,cell_num_y))
         for tx_idx in range(0,num_tx):
             # remove previous transmitter
             if scene.get(f"tx_{tx_idx-1}") is not None:
@@ -172,9 +112,9 @@ class SensingScene():
             rx_name_list = []
             rx_index_list = []
             # Compute paths for receivers
-            while i < cell_num:
+            while i < cell_num_x:
                 j = 0
-                while j < cell_num:
+                while j < cell_num_y:
                     # Add receiver
                     rx_name = f"rx_{i}_{j}"
                     rx_name_list.append(rx_name)
@@ -195,7 +135,10 @@ class SensingScene():
                             i_idx = rx_index_list[num_idx][0]
                             j_idx = rx_index_list[num_idx][1]
                             a_,tau_ = a[0,num_idx,0,0,0,:,0],tau[0,num_idx,0,:]
-                            self.crb_delay[tx_idx,i_idx,j_idx] = self._crb_delay(20,a_,tau_)
+                            if crb_method == 0:
+                                self.crb_delay[tx_idx,i_idx,j_idx] = self._crb_delay0(20,a_,tau_)
+                            elif crb_method == 1:
+                                self.crb_delay[tx_idx,i_idx,j_idx] = self._crb_delay1(20,a_,tau_)
                         rx_name_list = []
                         rx_index_list = []
                     j += 1
@@ -212,19 +155,104 @@ class SensingScene():
                     i_idx = rx_index_list[num_idx][0]
                     j_idx = rx_index_list[num_idx][1]
                     a_,tau_ = a[0,num_idx,0,0,0,:,0],tau[0,num_idx,0,:]
-                    self.crb_delay[tx_idx,i_idx,j_idx] = self._crb_delay(20,a_,tau_)
+                    if crb_method == 0:
+                        self.crb_delay[tx_idx,i_idx,j_idx] = self._crb_delay0(20,a_,tau_)
+                    elif crb_method == 1:
+                        self.crb_delay[tx_idx,i_idx,j_idx] = self._crb_delay1(20,a_,tau_)
                 rx_name_list = []
                 rx_index_list = []
         
         # rotate and flip
         for tx_idx in range(0,num_tx):
             crb_tmp = self.crb_delay[tx_idx,:,:]
-            crb_tmp = np.rot90(crb_tmp,k=1)
-            crb_tmp = np.flip(crb_tmp,axis=0)
+            crb_tmp = np.flip(crb_tmp,axis=1)
+            self.crb_delay[tx_idx,:,:] = crb_tmp
+        return self.crb_delay
+    
+    def compute_paths_crb(self,crb_method=0,max_depth=3,num_samples=1000000,max_rx_each_turn = 100,los=True,reflection=True,diffraction=False,scattering=False):
+        self._paths = None
+        scene = self._scene
+        cell_num_x = self._cell_num_x # Number of cells in the map
+        cell_num_y = self._cell_num_y # Number of cells in the map
+        cell_positions = self._cell_positions # 3D array of cell positions
+        num_tx = self._tx_positions.shape[0] # Number of transmitters
+        self.crb_delay = np.zeros((num_tx,cell_num_x,cell_num_y))
+        for tx_idx in range(0,num_tx):
+            # remove previous transmitter
+            if scene.get(f"tx_{tx_idx-1}") is not None:
+                scene.remove(f"tx_{tx_idx-1}")
+            # Add new transmitter
+            tx = Transmitter(name=f"tx_{tx_idx}",position=self._tx_positions[tx_idx,:])
+            if scene.get(f"tx_{tx_idx}") is not None:
+                scene.remove(f"tx_{tx_idx}")
+            scene.add(tx)
+            # Set transmitter look direction
+            rx_tmp = Receiver(name="rx_tmp",position=self._tx_look_dir[tx_idx,:])
+            tx.look_at(rx_tmp)
+            scene.remove("rx_tmp")
+            num = 0
+            i = 0
+            rx_name_list = []
+            rx_index_list = []
+            # Compute paths for receivers
+            while i < cell_num_x:
+                j = 0
+                while j < cell_num_y:
+                    # Add receiver
+                    rx_name = f"rx_{i}_{j}"
+                    rx_name_list.append(rx_name)
+                    rx_index_list.append([i,j])
+                    rx = Receiver(name=rx_name,position=cell_positions[i,j,:])
+                    if scene.get(rx_name) is not None:
+                        scene.remove(rx_name)
+                    scene.add(rx)
+                    num += 1
+                    if num % max_rx_each_turn == 0: # Compute paths
+                        # print(num,num // max_rx_each_turn)
+                        paths:mysionna.rt.Paths = scene.compute_paths(max_depth=max_depth,num_samples=num_samples,los=los,reflection=reflection,diffraction=diffraction,scattering=scattering)
+                        a,tau = paths.cir()
+                        print (num,a.shape)
+                        for rx_name in rx_name_list:
+                            scene.remove(rx_name)
+                        for num_idx in range(0,max_rx_each_turn):
+                            i_idx = rx_index_list[num_idx][0]
+                            j_idx = rx_index_list[num_idx][1]
+                            a_,tau_ = a[0,num_idx,0,0,0,:,0],tau[0,num_idx,0,:]
+                            if crb_method == 0:
+                                self.crb_delay[tx_idx,i_idx,j_idx] = self._crb_delay0(20,a_,tau_)
+                            elif crb_method == 1:
+                                self.crb_delay[tx_idx,i_idx,j_idx] = self._crb_delay1(20,a_,tau_)
+                        rx_name_list = []
+                        rx_index_list = []
+                    j += 1
+                i += 1
+            # Compute paths for the rest receivers
+            if num % max_rx_each_turn != 0:
+                paths:mysionna.rt.Paths = scene.compute_paths(max_depth=max_depth,num_samples=num_samples,los=los,reflection=reflection,diffraction=diffraction,scattering=scattering)
+                paths.normalize_delays = False
+                a,tau = paths.cir()
+                print (num,a.shape)
+                for rx_name in rx_name_list:
+                    scene.remove(rx_name)
+                for num_idx in range(0,num % max_rx_each_turn):
+                    i_idx = rx_index_list[num_idx][0]
+                    j_idx = rx_index_list[num_idx][1]
+                    a_,tau_ = a[0,num_idx,0,0,0,:,0],tau[0,num_idx,0,:]
+                    if crb_method == 0:
+                        self.crb_delay[tx_idx,i_idx,j_idx] = self._crb_delay0(20,a_,tau_)
+                    elif crb_method == 1:
+                        self.crb_delay[tx_idx,i_idx,j_idx] = self._crb_delay1(20,a_,tau_)
+                rx_name_list = []
+                rx_index_list = []
+        
+        # rotate and flip
+        for tx_idx in range(0,num_tx):
+            crb_tmp = self.crb_delay[tx_idx,:,:]
+            crb_tmp = np.flip(crb_tmp,axis=1)
             self.crb_delay[tx_idx,:,:] = crb_tmp
         return self.crb_delay
         
-    def _crb_delay(self,snr,a,tau):
+    def _crb_delay0(self,snr,a,tau):
         frequency = self._scene.frequency
         # tau = tau[a!=0]
         # a = a[a!=0]
@@ -269,12 +297,46 @@ class SensingScene():
         crb = tf.abs(tf.cast(B_diag,tf.complex128)) / tf.cast((8 * np.pi**2 * snr * frequency **2),tf.float64)
         a_sortidx = np.argsort(np.abs(a))
         return crb[a_sortidx[-1]]
+    
+    def _crb_delay1(self,snr,a,tau):
+        frequency = self._scene.frequency
+        tau = tau[a!=0]
+        a = a[a!=0]
+        a = a[tau>0]
+        tau = tau[tau>0]
+        if len(a) == 0:
+            return 0
+        phase = tf.complex(tf.zeros_like(tau),2*np.pi*frequency*tau)
+        e = tf.exp(phase)
+        a = a * e
+        length = len(a)
+        if length == 0:
+            return 0
+        tau_i = tf.repeat(tau,length)
+        tau_i = tf.reshape(tau_i, (length,length))
+        tau_j = tf.transpose(tau_i)
+        tau_i_mine_j = tau_i- tau_j
+        tau_i_mul_j = tau_i* tau_j
+        alpha_ij = tf.reshape(a, (length,1)) @ tf.reshape(a, (1,length))
+        one = tf.ones((length,length))
+        F_alpha= 2*snr*tf.math.abs(alpha_ij)/(tau_i_mul_j**2)
+        F_cos = (one+4*(np.pi**2)*(frequency) * tau_i_mul_j)*tf.math.cos(2*np.pi*frequency*tau_i_mine_j)
+        F_sin = 2*np.pi*frequency*tau_i_mine_j*tf.math.sin(2*np.pi*frequency*tau_i_mine_j)
+        F = F_alpha*(F_cos+F_sin)
+        try:
+            crb_F = tf.linalg.inv(F)
+        except:
+            return 0
+        crb = tf.linalg.diag_part(crb_F)
+        a_sortidx = np.argsort(np.abs(a))
+        return crb[a_sortidx[-1]]
         
     def render(self,heat=False,delay=True,angle=False,speed=False):
-        cell_num = self.cell_num # Number of cells in the map
+        cell_num_x = self._cell_num_x # Number of cells in the map
+        cell_num_y = self._cell_num_y # Number of cells in the map
         cell_positions = self._cell_positions
         scene = self._scene
-        colors = np.zeros((cell_num,cell_num,3))
+        colors = np.zeros((cell_num_x,cell_num_y,3))
         if heat:
             if delay:
                 if self.crb_delay is None:
@@ -286,8 +348,8 @@ class SensingScene():
                 if self.crb_speed is None:
                     raise Exception("Please compute CRB first.")
         
-        for i in range(0, cell_num):
-            for j in range(0, cell_num):
+        for i in range(0, cell_num_x):
+            for j in range(0, cell_num_y):
                 rx_name = f"rx_{i}_{j}"
                 if scene.get(rx_name) is not None:
                     scene.remove(rx_name)
@@ -301,7 +363,8 @@ class SensingScene():
             scene.add(tx)
     
     def heat_map(self,tx=0,delay=True,angle=False,speed=False):
-        cell_num = self.cell_num  
+        cell_num_x = self._cell_num_x  
+        cell_num_y = self._cell_num_y
         num_pic = 0
         pic_id = 1
         fig = plt.figure()
@@ -313,13 +376,13 @@ class SensingScene():
             num_pic = num_pic + 1
         if delay:
             if self.crb_delay is None:
-                raise Exception("Please compute CRB first.")
+                raise Exception("Please compute CRB_delay first.")
             crb = self.crb_delay[tx,:,:]
             plt.subplot(num_pic,1,pic_id)
             pic_id = pic_id + 1
             plt.imshow(np.log10(crb),origin='lower')
             plt.colorbar()
-            tx_position = (self._tx_positions[tx,:]-self.map_position)/self.cell_size
+            tx_position = (self._tx_positions[tx,:] - self._map_center + np.array([self._map_size_y/2,self._map_size_x/2,0])) / self._cell_size
             plt.scatter(tx_position[0],tx_position[1],marker='x',color='r')
         
         return fig
